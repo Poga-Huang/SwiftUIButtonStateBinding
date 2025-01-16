5b25adb691e10623150f05c5bd0ea021c4912ce6
poga07 <chris@168money.com.tw>
2025-01-16 16:36:34 +0800

    Initial Commit

---
+++ SwiftUIButtonStateBinding/SwiftUIButtonStateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonStateBindingApp.swift
+//  SwiftUIButtonStateBinding
+//
+//  Created by 黃柏嘉 on 2025/01/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonStateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
